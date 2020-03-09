7338ca34b3049d15beed1ea081fb058ab20eefa6
daonez <cyongchoi@gmail.com>
2020-03-10 00:23:48 +0900

    imported fonts to global style component, import reset css to styles component

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import GlobalStyles from "Fonts/Fonts";
+import GlobalStyles from "Styles/GlobalStyles";
 import styled from "styled-components";
 
 export default class Login extends Component {
@@ -7,7 +7,10 @@ export default class Login extends Component {
     return (
       <div>
         <GlobalStyles />
-        <AwesomeHeadline>Hello</AwesomeHeadline>
+        <AwesomeHeadline>Bold</AwesomeHeadline>
+        <AwesomeHeadline2> A Light</AwesomeHeadline2>
+        <AwesomeHeadline3>A Regular</AwesomeHeadline3>
+        <AwesomeHeadline4> A Thin</AwesomeHeadline4>
       </div>
     );
   }
@@ -15,4 +18,18 @@ export default class Login extends Component {
 
 const AwesomeHeadline = styled.h1`
   font-family: Spoqa Han Sans Bold;
+  font-size: 100px;
+`;
+
+const AwesomeHeadline2 = styled.h2`
+  font-family: Spoqa Han Sans Light;
+  font-size: 100px;
+`;
+const AwesomeHeadline3 = styled.h3`
+  font-family: Spoqa Han Sans Regular;
+  font-size: 100px;
+`;
+const AwesomeHeadline4 = styled.h4`
+  font-family: Spoqa Han Sans Thin;
+  font-size: 100px;
 `;
--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -1,5 +1,9 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import Bold from "Fonts/Spoqa-Han-Sans-Bold.woff";
+import Light from "Fonts/Spoqa-Han-Sans-Light.woff";
+import Regular from "Fonts/Spoqa-Han-Sans-Regular.woff";
+import Thin from "Fonts/Spoqa-Han-Sans-Thin.woff";
 
 const GlobalStyles = createGlobalStyle`
 ${reset};
@@ -7,8 +11,23 @@ ${reset};
     font-family:'Spoqa Han Sans Bold';
     src: local('Spoqa Han Sans Bold'),
       url(${Bold}) format('woff');
-      font-weight:500;
-      font-style:Bold;
-}
+      font-size:100px
+},
+@font-face{
+    font-family:'Spoqa Han Sans Light';
+    src: local('Spoqa Han Sans Light'),
+      url(${Light}) format('woff');
+},
+@font-face{
+    font-family:'Spoqa Han Sans Regular';
+    src: local('Spoqa Han Sans Regular'),
+      url(${Regular}) format('woff');
+},
+@font-face{
+    font-family:'Spoqa Han Sans Thin';
+    src: local('Spoqa Han Sans Thin'),
+      url(${Thin}) format('woff');
+},
 `;
+
 export default GlobalStyles;
