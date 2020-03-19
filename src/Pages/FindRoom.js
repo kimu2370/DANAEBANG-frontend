07ffdc64c50b0f1229c694a9cc3d705606e4feb7
bearsjelly <59649268+bearsjelly@users.noreply.github.com>
2020-03-19 20:22:48 +0900

    map dragend 이벤트 적용

--- src/Pages/FindRoom.js
+++ src/Pages/FindRoom.js
@@ -1,26 +1,74 @@
 import React, { Component } from "react";
-import FindRoomTop from "./components/FindRoomTop";
-import RoomContent from "./components/RoomContent";
-import MapContent from "./components/MapContent";
-
+import FindRoomTop from "Pages/Findroom/components/FindRoomTop";
+import RoomContent from "Pages/Findroom/components/RoomContent";
+import MapContent from "Pages/Findroom/components/MapContent";
+import Layout from "Components/Layout/Layout";
 import styled from "styled-components";
 
 class FindRoom extends Component {
+  constructor() {
+    super();
+    this.state = {
+      lat: 37.506502,
+      lng: 127.053617,
+      roomData: [],
+      roomlength: ""
+    };
+  }
+
+  componentDidMount() {
+    this.roomDataFetch();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.lat !== this.state.lat || prevState.lng !== this.state.lng) {
+      this.setState({
+        roomData: []
+      });
+      this.roomDataFetch();
+    }
+  }
+
+  roomDataFetch = () => {
+    fetch(
+      `http://52.78.11.154:8000/room/list?latitude=${this.state.lat}&longitude=${this.state.lng}&zoom=1&offset=1&limit=24&multi_room_type=1&multi_room_type=2&multi_room_type=3&multi_room_type=4&multi_room_type=5&selling_type=1&selling_type=2&selling_type=3&room_size=0&room_size=50&maintenance_price=0&maintenance_price=10&deposit_range=0&deposit_range=20000&fee_range=0&fee_range=200`,
+      { method: "GET" }
+    )
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          roomData: res.results
+        });
+      });
+  };
+
+  //map과 연결하여 state 관리하는 함수
+  parentsData = (lat, lng, roomlength) => {
+    this.setState({ lat, lng, roomlength });
+  };
+
   render() {
     return (
-      <div className="FindRoom">
-        <FindRoomTop />
-        <FindRoomContentWrap>
-          <RoomListWrap>
-            <RoomList>
-              <RoomContent />
-            </RoomList>
-          </RoomListWrap>
-          <MapWrap>
-            <MapContent />
-          </MapWrap>
-        </FindRoomContentWrap>
-      </div>
+      <>
+        <Layout>
+          <FindRoomTop />
+          <FindRoomContentWrap>
+            <RoomListWrap>
+              <RoomList>
+                {this.state.roomData && (
+                  <RoomContent
+                    roomDatafromparents={this.state.roomData}
+                    roomlength={this.state.roomlength}
+                  />
+                )}
+              </RoomList>
+            </RoomListWrap>
+            <MapWrap>
+              <MapContent mapdatafromparents={this.parentsData} />
+            </MapWrap>
+          </FindRoomContentWrap>
+        </Layout>
+      </>
     );
   }
 }
@@ -32,6 +80,8 @@ const FindRoomContentWrap = styled.div`
 `;
 const RoomListWrap = styled.div`
   width: 60%;
+  height: 100vh;
+  overflow-y: scroll;
 `;
 const RoomList = styled.div`
   width: 100%;
@@ -39,7 +89,7 @@ const RoomList = styled.div`
 `;
 const MapWrap = styled.div`
   width: 40%;
-  height: 910px;
+  height: 100vh;
 `;
 
 export default FindRoom;
